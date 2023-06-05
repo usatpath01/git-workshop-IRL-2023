1cb31c9556a1e41c421ec8bbaef5c3290da014a7
usatpath01 <utkalika.satapathy01@gmail.com>
2023-06-05 16:21:52 +0530

    fix in func1

--- docs/install.md
+++ docs/install.md
@@ -1,1 +1,7 @@
+pre line 1
+pre line 2
+
 ## Hello World
+
+post line 1
+post line 2
--- helloworld.c
+++ helloworld.c
@@ -1,5 +1,10 @@
 #include<stdio.h>
 
+int fuc1(){
+    printf("hello world");
+    return 0;
+}
+
 //comment
 int main(){
     printf("hello world");
